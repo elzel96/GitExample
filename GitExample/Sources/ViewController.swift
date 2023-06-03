3dafa116e42d39032f7dd51c797b22132a93a085
elzel <elzel666@gmail.com>
2023-06-03 23:58:33 +0300

    create_someview
    создали новый view

--- GitExample/Sources/ViewController.swift
+++ GitExample/Sources/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var someView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
